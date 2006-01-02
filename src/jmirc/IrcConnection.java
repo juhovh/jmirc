452da5d6c92d10fc9c23fb62b1721821c8520795
Juho Vähä-Herttua <juhovh@iki.fi>
2006-01-02 17:03:01 +0000

    Initial revision

---
+++ src/jmirc/IrcConnection.java
@@ -0,0 +1,221 @@
+/*
+Copyright (C) 2004  Juho Vähä-Herttua
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+package jmirc;
+
+import java.io.*;
+import java.util.*;
+
+public abstract class IrcConnection {
+	// We save about 1 kB when inputting these as strings instead of arrays
+	protected char[] koi8rmap = "\u2500\u2502\u250C\u2510\u2514\u2518\u251C\u2524\u252C\u2534\u253C\u2580\u2584\u2588\u258C\u2590\u2591\u2592\u2593\u2320\u25A0\u2219\u221A\u2248\u2264\u2265\u00A0\u2321\u00B0\u00B2\u00B7\u00F7\u2550\u2551\u2552\u0451\u2553\u2554\u2555\u2556\u2557\u2558\u2559\u255A\u255B\u255C\u255D\u255E\u255F\u2560\u2561\u0401\u2562\u2563\u2564\u2565\u2566\u2567\u2568\u2569\u256A\u256B\u256C\u00A9\u044E\u0430\u0431\u0446\u0434\u0435\u0444\u0433\u0445\u0438\u0439\u043A\u043B\u043C\u043D\u043E\u043F\u044F\u0440\u0441\u0442\u0443\u0436\u0432\u044C\u044B\u0437\u0448\u044D\u0449\u0447\u044A\u042E\u0410\u0411\u0426\u0414\u0415\u0424\u0413\u0425\u0418\u0419\u041A\u041B\u041C\u041D\u041E\u041F\u042F\u0420\u0421\u0422\u0423\u0416\u0412\u042C\u042B\u0417\u0428\u042D\u0429\u0427\u042A".toCharArray();
+	// notes: #154 is a non-breaking space
+	protected char[] cp1251map = "\u0402\u0403\u201A\u0453\u201E\u2026\u2020\u2021\u20AC\u2030\u0409\u2039\u040A\u040C\u040B\u040F\u0452\u2018\u2019\u201C\u201D\u2022\u2013\u2014\uFFFD\u2122\u0459\u203A\u045A\u045C\u045B\u045F\u00A0\u040E\u045E\u0408\u00A4\u0490\u00A6\u00A7\u0401\u00A9\u0404\u00AB\u00AC\u00AD\u00AE\u0407\u00B0\u00B1\u0406\u0456\u0491\u00B5\u00B6\u00B7\u0451\u2116\u0454\u00BB\u0458\u0405\u0455\u0457\u0410\u0411\u0412\u0413\u0414\u0415\u0416\u0417\u0418\u0419\u041A\u041B\u041C\u041D\u041E\u041F\u0420\u0421\u0422\u0423\u0424\u0425\u0426\u0427\u0428\u0429\u042A\u042B\u042C\u042D\u042E\u042F\u0430\u0431\u0432\u0433\u0434\u0435\u0436\u0437\u0438\u0439\u043A\u043B\u043C\u043D\u043E\u043F\u0440\u0441\u0442\u0443\u0444\u0445\u0446\u0447\u0448\u0449\u044A\u044B\u044C\u044D\u044E\u044F".toCharArray();
+	// notes: #152 not used in cp1252 so we encode it to '�'
+	//        #160 is a non-breaking space, #173 is a soft hyphen
+	protected Hashtable hashmap = null;
+
+	public abstract String connect(String host, int port, String init);
+	public abstract void disconnect();
+
+	public abstract String updateConnection();
+	public abstract String readLine();
+	public abstract String writeData(String input);
+	public abstract boolean hasDataInBuffer();
+	
+	public abstract boolean isConnected();
+	public abstract int getBytesIn();
+	public abstract int getBytesOut();
+	public abstract void setUnicodeMode(boolean utf8detect, boolean utf8output);
+
+	protected String byteArrayToString(byte[] bytes, String encoding, boolean utf8fallback) {
+		char[] map = null;
+		String ret;
+
+		if (utf8fallback) {
+			// we need to have some 1-byte fallback, let's use latin1
+			if (encoding.equals("UTF-8"))
+				encoding = "ISO-8859-1";
+
+			try {
+				ret = decodeUTF8(bytes, false);
+				return ret;
+			} catch (UTFDataFormatException udfe) {}
+		}
+
+		if (encoding.equals("KOI8-R"))
+			map = koi8rmap;
+		else if (encoding.equals("Windows-1251"))
+			map = cp1251map;
+
+		if (map != null) {
+			char[] chars = new char[bytes.length];
+			for (int i=0; i<bytes.length; i++) {
+				byte b = bytes[i];
+				chars[i] = (b >= 0) ? (char) b : map[b+128];
+			}
+			ret = new String(chars);
+		}
+		else if (encoding.equals("UTF-8")) {
+			try {
+				ret = decodeUTF8(bytes, true);
+			} catch (UTFDataFormatException udfe) {
+				// this should never happen when gracious decoding is true
+				ret = new String(bytes);
+			}
+		}
+		else {
+			try {
+				ret = new String(bytes, encoding);
+			} catch (UnsupportedEncodingException uee) {
+				ret = new String(bytes);
+			}
+		}
+
+		return ret;
+	}
+
+	protected byte[] stringToByteArray(String string, String encoding, boolean utf8output) {
+		byte[] ret;
+
+		if (utf8output)
+			encoding = "UTF-8";
+
+		if (encoding.equals("KOI8-R") || encoding.equals("Windows-1251")) {
+			if (hashmap == null || !encoding.equals((String) hashmap.get("encoding"))) {
+				if (encoding.equals("KOI8-R"))
+					hashmap = generateHashmap(koi8rmap);
+				else if (encoding.equals("Windows-1251"))
+					hashmap = generateHashmap(cp1251map);
+				hashmap.put("encoding", encoding);
+			}
+
+			char[] chars = string.toCharArray();
+			byte[] bytes = new byte[chars.length];
+
+			for (int i=0; i<chars.length; i++) {
+				if (chars[i]<0x80)
+					bytes[i] = (byte) chars[i];
+				else {
+					Byte b = (Byte) hashmap.get(new Character(chars[i]));
+					bytes[i] = (b == null) ? (byte) 0x3f : b.byteValue(); // 0x3f is '?'
+				}
+			}
+
+			ret = bytes;
+		}
+		else if (encoding.equals("UTF-8")) {
+			ret = encodeUTF8(string);
+		}
+		else {
+			try {
+				ret = string.getBytes(encoding);
+			} catch (UnsupportedEncodingException uee) {
+				ret = string.getBytes();
+			}
+		}
+
+		return ret;
+	}
+
+	private Hashtable generateHashmap(char[] encmap) {
+		Hashtable ret = new Hashtable();
+
+		for (int i=0; i<encmap.length; i++)
+			ret.put(new Character(encmap[i]), new Byte((byte) (0x80+i)));
+
+		return ret;
+	}
+
+	private byte[] encodeUTF8(String text) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		byte[] ret;
+		
+		for (int i=0; i<text.length(); i++) {
+			char c = text.charAt(i);
+			if (c != '\u0000' && c < '\u0080')
+				baos.write(c);
+			else if (c == '\u0000' || (c >= '\u0080' && c < '\u0800')) {
+				baos.write((byte)(0xc0 | (0x1f & (c >> 6))));
+				baos.write((byte)(0x80 | (0x3f & c)));
+			}
+			else {
+				baos.write((byte)(0xe0 | (0x0f & (c >> 12))));
+				baos.write((byte)(0x80 | (0x3f & (c >>  6))));
+				baos.write((byte)(0x80 | (0x3f & c)));
+			}
+		}
+		ret = baos.toByteArray();
+		
+		return ret;
+	}
+	
+	private String decodeUTF8(byte[] data, boolean gracious) throws UTFDataFormatException {
+		byte a, b, c;
+		StringBuffer ret = new StringBuffer();
+		
+		try {
+			for (int i=0; i<data.length; i++) {
+				a = data[i];
+				if ((a&0x80) == 0)
+					ret.append((char) a);
+				else if ((a&0xe0) == 0xc0) {
+					b = data[++i];
+					if ((b&0xc0) == 0x80)
+						ret.append((char)(((a& 0x1F) << 6) | (b & 0x3F)));
+					else {
+						if (gracious) {
+							ret.append("?");
+							i -= 1;
+						}
+						else
+							throw new UTFDataFormatException("Illegal 2-byte group");
+					}
+				}
+				else if ((a&0xf0) == 0xe0) {
+					b = data[++i];
+					c = data[++i];
+					if (((b&0xc0) == 0x80) && ((c&0xc0) == 0x80)) {
+						ret.append((char)(((a & 0x0F) << 12) | ((b & 0x3F) << 6) | (c & 0x3F)));
+					}
+					else {
+						if (gracious) {
+							ret.append("?");
+							i -= 2;
+						}
+						else
+							throw new UTFDataFormatException("Illegal 3-byte group");
+					}
+				}
+				else if (((a&0xf0) == 0xf0) || ((a&0xc0) == 0x80)) {
+					if (gracious)
+						ret.append("?");
+					else
+						throw new UTFDataFormatException("Illegal first byte of a group");
+				}
+			}
+		} catch (ArrayIndexOutOfBoundsException aioobe) {
+			if (gracious)
+				ret.append("?");
+			else
+				throw new UTFDataFormatException("Unexpected EOF");
+		}
+		
+		return ret.toString();
+	}
+}
---
+++ src/jmirc/jmIrc.java
@@ -0,0 +1,413 @@
+/*
+Copyright (C) 2004  Juho Vähä-Herttua
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, 
+USA.
+*/
+
+package jmirc;
+
+import java.util.*;
+import javax.microedition.midlet.*;
+import javax.microedition.lcdui.*;
+
+public class jmIrc extends MIDlet implements CommandListener {
+	public final static String VERSION = "0.95";
+
+	private final static int FORM_MAIN = 0;
+	private final static int FORM_PROFILES = 1;
+	private final static int FORM_CONFIG = 2;
+	private final static int FORM_CONFIG_EDIT = 3;
+	private final static int FORM_ADVANCED = 4;
+	private final static int FORM_HTTP = 5;
+
+	private Display display;
+	protected static Form mainform;
+	private int currentform;
+	private boolean running;
+
+	// these are initialized on connection time
+	private static IrcConnection irc;
+	private static Listener listener; // irc listener
+
+	private static UIHandler uihandler;
+	private static Database db;
+
+	private Command cmd_connect, cmd_profiles, cmd_advanced, cmd_http, cmd_exit;
+	private Command cmd_profile_add, cmd_profile_edit, cmd_profile_delete;
+	private Command cmd_ok, cmd_cancel;
+
+	private TextField tf_profilename, tf_nick, tf_altnick, tf_host, tf_port, tf_channels, tf_username, tf_realname;
+	private TextField tf_hilight, tf_passwd, tf_buflines;
+	private TextField tf_gwhost, tf_gwport, tf_gwpasswd, tf_polltime;
+
+	private ChoiceGroup cg_misc, cg_encoding;
+	private ChoiceGroup cg_usehttp;
+
+	private List list_profile;
+
+	public jmIrc() {
+		display = Display.getDisplay(this);
+
+		cmd_connect = new Command("Connect", Command.OK, 1);
+		cmd_profiles = new Command("Profiles", Command.SCREEN, 2);
+		cmd_advanced = new Command("Advanced", Command.SCREEN, 3);
+		cmd_http = new Command("HTTP Config", Command.SCREEN, 4);
+		cmd_exit = new Command("Exit", Command.EXIT, 6);
+
+		cmd_profile_add = new Command("Add new profile", Command.SCREEN, 2);
+		cmd_profile_edit = new Command("Edit profile", Command.SCREEN, 3);
+		cmd_profile_delete = new Command("Delete profile", Command.SCREEN, 4);
+
+		cmd_ok = new Command("Save", Command.OK, 1);
+		cmd_cancel = new Command("Cancel", Command.EXIT, 10);
+
+		mainform = new Form("jmIrc");
+		mainform.append("jmIrc " + VERSION + "\n");
+		mainform.append("By juhovh");
+		mainform.addCommand(cmd_connect);
+		mainform.addCommand(cmd_profiles);
+		mainform.addCommand(cmd_advanced);
+		mainform.addCommand(cmd_http);
+		mainform.addCommand(cmd_exit);
+		mainform.setCommandListener(this);
+
+		db = new Database();
+		running = false;
+	}
+
+	public void startApp() {
+		if (!running) {
+			db.load();
+			display.setCurrent(mainform);
+			currentform = FORM_MAIN;
+			running = true;
+		}
+	}
+
+	public void commandAction(Command cmd, Displayable disp) {
+		if (cmd == cmd_connect) {
+			if (db.usehttp)
+				irc = (IrcConnection) new HttpIrc(db.gwhost, db.gwport, db.gwpasswd, db.encoding);
+			else
+				irc = (IrcConnection) new SocketIrc(db.usepoll, db.encoding);
+
+			irc.setUnicodeMode(db.utf8detect, db.utf8output);
+			uihandler = new UIHandler(db, display);
+			uihandler.setDisplay(uihandler.getConsole());
+			listener = new Listener(db, irc, uihandler);
+			listener.start();
+			System.gc();
+		}
+		else if (cmd == cmd_exit) {
+			try {
+				destroyApp(true);
+			} catch (MIDletStateChangeException msce) { ; } // this never happens
+			notifyDestroyed();
+		}
+		else if ((cmd == cmd_ok || cmd == cmd_cancel || cmd == List.SELECT_COMMAND) && currentform != FORM_MAIN) {
+			if (currentform == FORM_PROFILES) {
+				currentform = FORM_MAIN;
+
+				db.profileidx = list_profile.getSelectedIndex();
+				db.save_profile();
+				list_profile = null;
+
+				db.setProfile(db.profileidx);
+				display.setCurrent(mainform);
+			}
+			else if (currentform == FORM_CONFIG || currentform == FORM_CONFIG_EDIT) {
+				boolean editing = (currentform == FORM_CONFIG_EDIT);
+
+				if (cmd == cmd_ok) {
+					if (tf_nick.getString().equals("")) {
+						Alert a = new Alert("Warning", "Nick must be set", null, AlertType.WARNING);
+						a.setTimeout(a.FOREVER);
+						display.setCurrent(a);
+						return;
+					}
+					currentform = FORM_PROFILES;
+					db.profilename = tf_profilename.getString();
+					db.nick = tf_nick.getString();
+					db.altnick = tf_altnick.getString();
+					db.host = tf_host.getString();
+					db.port = parseInt(tf_port.getString());
+					db.channels = tf_channels.getString();
+					db.username = tf_username.getString();
+					db.realname = tf_realname.getString();
+					db.passwd = tf_passwd.getString();
+
+					if (editing)
+						db.editProfile(list_profile.getSelectedIndex());
+					else
+						db.addProfile();
+				}
+				currentform = FORM_PROFILES;
+				tf_profilename = null;
+				tf_nick = null;
+				tf_altnick = null;
+				tf_host = null;
+				tf_port = null;
+				tf_channels = null;
+				tf_username = null;
+				tf_realname = null;
+				tf_passwd = null;
+
+				if (cmd == cmd_ok)
+					commandAction(cmd_profiles, null);
+				else
+					display.setCurrent(list_profile);
+			}
+			else if (currentform == FORM_ADVANCED) {
+				currentform = FORM_MAIN;
+				if (cmd == cmd_ok) {
+					db.header = cg_misc.isSelected(0);
+					db.timestamp = cg_misc.isSelected(1);
+					db.usecolor = cg_misc.isSelected(2);
+					db.usemirccol = cg_misc.isSelected(3);
+					db.usepoll = cg_misc.isSelected(4);
+					db.showinput = cg_misc.isSelected(5);
+					db.utf8detect = cg_misc.isSelected(6);
+					db.utf8output = cg_misc.isSelected(7);
+					db.encoding = cg_encoding.getString(cg_encoding.getSelectedIndex());
+					db.buflines = parseInt(tf_buflines.getString());
+					db.hilight = tf_hilight.getString();
+					db.save_advanced();
+				}
+				cg_misc = null;
+				cg_encoding = null;
+				tf_buflines = null;
+				tf_hilight = null;
+				display.setCurrent(mainform);
+			}
+			else if (currentform == FORM_HTTP) {
+				currentform = FORM_MAIN;
+				if (cmd == cmd_ok) {
+					db.usehttp = cg_usehttp.isSelected(0);
+					db.gwhost = tf_gwhost.getString();
+					db.gwport = parseInt(tf_gwport.getString());
+					db.gwpasswd = tf_gwpasswd.getString();
+					db.polltime = parseInt(tf_polltime.getString());
+					db.save_http();
+				}
+				cg_usehttp = null;
+				tf_gwhost = null;
+				tf_gwport = null;
+				tf_gwpasswd = null;
+				tf_polltime = null;
+				display.setCurrent(mainform);
+			}
+		}
+		else {
+			Form cfgform;
+
+			if (cmd == cmd_profiles) {
+				String[] profiles;
+
+				profiles = db.getProfiles();
+				list_profile = new List("Profiles", List.IMPLICIT);
+				for (int i=0; i<profiles.length; i++) {
+					list_profile.append(profiles[i], null);
+				}
+				if (db.profileidx >= 0)
+					list_profile.setSelectedIndex(db.profileidx, true);
+
+				list_profile.addCommand(cmd_profile_add);
+				list_profile.addCommand(cmd_profile_edit);
+				list_profile.addCommand(cmd_profile_delete);
+				list_profile.addCommand(cmd_ok);
+
+				list_profile.setCommandListener(this);
+
+				display.setCurrent(list_profile);
+				currentform = FORM_PROFILES;
+				return;
+			}
+			else if (cmd == cmd_profile_add || cmd == cmd_profile_edit) {
+				if (cmd == cmd_profile_edit)
+					db.setProfile(list_profile.getSelectedIndex());
+				else
+					db.setProfile(-1);
+
+				tf_profilename = new TextField("Profile name", db.profilename, 10, TextField.ANY);
+				tf_nick = new TextField("Nick", db.nick, 20, TextField.ANY);
+				tf_altnick = new TextField("Alternative nick", db.altnick, 20, TextField.ANY);
+				tf_host = new TextField("IRC server", db.host, 200, TextField.URL);
+				tf_port = new TextField("IRC server port", new Integer(db.port).toString(), 5, TextField.NUMERIC);
+				tf_channels = new TextField("Channels", db.channels, 600, TextField.ANY);
+				tf_username = new TextField("Username", db.username, 8, TextField.ANY);
+				tf_realname = new TextField("Real name", db.realname, 50, TextField.ANY);
+				tf_passwd = new TextField("Server password", db.passwd, 10, TextField.PASSWORD);
+
+				cfgform = new Form("Config");
+				cfgform.append(tf_profilename);
+				cfgform.append(tf_nick);
+				cfgform.append(tf_altnick);
+				cfgform.append(tf_host);
+				cfgform.append(tf_port);
+				cfgform.append(tf_channels);
+				cfgform.append(tf_username);
+				cfgform.append(tf_realname);
+				cfgform.append(tf_passwd);
+
+				if (cmd == cmd_profile_edit)
+					currentform = FORM_CONFIG_EDIT;
+				else
+					currentform = FORM_CONFIG;
+			}
+			else if (cmd == cmd_profile_delete) {
+				db.deleteProfile(list_profile.getSelectedIndex());
+				commandAction(cmd_profiles, null);
+				return;
+			}
+			else if (cmd == cmd_advanced) {
+				cg_misc = new ChoiceGroup("Misc settings", ChoiceGroup.MULTIPLE);
+				cg_misc.append("Use status header", null);
+				cg_misc.append("Use timestamp", null);
+				cg_misc.append("Use colours", null);
+				cg_misc.append("Use mIRC colours", null);
+				cg_misc.append("Use socket poll", null);
+				cg_misc.append("Print unhandled input", null);
+				cg_misc.append("Automatically detect UTF-8", null);
+				cg_misc.append("Always output UTF-8", null);
+
+				cg_misc.setSelectedIndex(0, db.header);
+				cg_misc.setSelectedIndex(1, db.timestamp);
+				cg_misc.setSelectedIndex(2, db.usecolor);
+				cg_misc.setSelectedIndex(3, db.usemirccol);
+				cg_misc.setSelectedIndex(4, db.usepoll);
+				cg_misc.setSelectedIndex(5, db.showinput);
+				cg_misc.setSelectedIndex(6, db.utf8detect);
+				cg_misc.setSelectedIndex(7, db.utf8output);
+
+				cg_encoding = new ChoiceGroup("Character encoding", ChoiceGroup.EXCLUSIVE);
+				cg_encoding.append("ISO-8859-1", null);
+				cg_encoding.append("ISO-8859-2", null);
+				cg_encoding.append("UTF-8", null);
+				cg_encoding.append("KOI8-R", null);
+				cg_encoding.append("Windows-1251", null);
+				if (db.encoding.equals("ISO-8859-1")) cg_encoding.setSelectedIndex(0, true);
+				else if (db.encoding.equals("ISO-8859-2")) cg_encoding.setSelectedIndex(1, true);
+				else if (db.encoding.equals("UTF-8")) cg_encoding.setSelectedIndex(2, true);
+				else if (db.encoding.equals("KOI8-R")) cg_encoding.setSelectedIndex(3, true);
+				else if (db.encoding.equals("Windows-1251")) cg_encoding.setSelectedIndex(4, true);
+				else cg_encoding.setSelectedIndex(0, true);
+
+				tf_buflines = new TextField("Backbuffer lines", new Integer(db.buflines).toString(), 3, TextField.NUMERIC);
+				tf_hilight = new TextField("Highlight string", db.hilight, 50, TextField.ANY);
+
+				cfgform = new Form("Advanced Config");
+				cfgform.append(cg_misc);
+				cfgform.append(cg_encoding);
+				cfgform.append(tf_buflines);
+				cfgform.append(tf_hilight);
+				currentform = FORM_ADVANCED;
+			}
+			else if (cmd == cmd_http) {				
+				cg_usehttp = new ChoiceGroup("HTTP", ChoiceGroup.MULTIPLE);
+				cg_usehttp.append("Use HTTP proxy server", null);
+				cg_usehttp.setSelectedIndex(0, db.usehttp);
+
+				tf_gwhost = new TextField("Proxy server", db.gwhost, 200, TextField.URL);
+				tf_gwport = new TextField("Proxy port", new Integer(db.gwport).toString(), 5, TextField.NUMERIC);
+				tf_gwpasswd = new TextField("Proxy password", db.gwpasswd, 10, TextField.PASSWORD);
+				tf_polltime = new TextField("HTTP poll time (sec):", new Integer(db.polltime).toString(), 2, TextField.NUMERIC);
+
+				cfgform = new Form("HTTP Config");
+				cfgform.append(cg_usehttp);
+				cfgform.append(tf_gwhost);
+				cfgform.append(tf_gwport);
+				cfgform.append(tf_gwpasswd);
+				cfgform.append(tf_polltime);
+				currentform = FORM_HTTP;
+			}
+			else return;
+
+			cfgform.addCommand(cmd_ok);
+			cfgform.addCommand(cmd_cancel);
+			cfgform.setCommandListener(this);
+
+			display.setCurrent(cfgform);
+		}
+	}
+
+	/**
+	 * Time to pause, free any space we don't need right now.
+	 */
+	public void pauseApp() {
+	}
+
+	/**
+	 * Destroy must cleanup everything.
+	 */
+	public void destroyApp(boolean unconditional) throws MIDletStateChangeException {
+		if (irc != null && irc.isConnected()) {
+			if (!unconditional)
+				throw new MIDletStateChangeException("IRC is still connected");
+
+			jmIrc.disconnect("QUIT :jmIrc destroyed by the OS");
+		}
+		if (db.usehttp) {
+			try {Thread.sleep(1000);} catch (InterruptedException ie) {}
+		}
+		//if (uihandler != null) uihandler.cleanup();
+	}
+	
+
+	private int parseInt(String input) {
+		int ret = 0;
+
+		try {
+			ret = Integer.parseInt(input);
+		} catch (NumberFormatException nfe) {}
+
+		return ret;
+	}
+
+
+	// Static functions called from other classes
+	public static void writeLine(String message) {
+		if (irc.isConnected()) {
+			String ret = irc.writeData(message + "\r\n");
+			if (ret != null) {
+				uihandler.getConsole().writeInfo(ret);
+			}
+			listener.setNeedUpdate(true);
+		}
+	}
+
+	public static void forceUpdate() {
+		listener.setNeedUpdate(true);
+	}
+
+	public static void disconnect(String quitmessage) {
+		if (irc.isConnected()) {
+			String ret = irc.writeData(quitmessage + "\r\n");
+			if (ret != null) {
+				uihandler.getConsole().writeInfo(ret);
+			}
+			irc.disconnect();
+			listener.setNeedUpdate(true);
+		}
+	}
+
+	public static int getBytesIn() {
+		return irc.getBytesIn();
+	}
+
+	public static int getBytesOut() {
+		return irc.getBytesOut();
+	}
+}
